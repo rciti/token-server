2d36865e1377baa99e990db827c3d46cda9586da
asiripanich <17020181+asiripanich@users.noreply.github.com>
2021-08-11 23:45:33 +1000

    Update the start shell script.
    - Bind a token file instead of copying it into the container.

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 docker build -t token-server .
 docker stop token-server
 docker rm token-server
-docker run -d -p 5000:5000 --name=token-server token-server
+docker run -d \
+    -p 5000:5000 \
+    --mount type=bind,source="$(pwd)"/tokens.txt,target=/usr/src/token-server/tokens.txt \
+    --name=token-server token-server
